d780f8c8a1c02dfa39161666422d1e6c98bf638c
YeWon Seo <syw1236@naver.com>
2023-12-16 19:27:05 +0900

    Create app.py
    플라스크 앱 파일

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask,render_template,request
+app = Flask(__name___)
+
+@app.route('/')
+def index():
+  return render_template('waterWeb.html')
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0',port=8083)
